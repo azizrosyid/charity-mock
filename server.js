ac398e0fe1f8d071e896c94cd1bffb1d3aa03e6b
azizrosyid <azizrosyid783@gmail.com>
2024-10-13 15:32:45 +0700

    Add bodyParser middleware to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = jsonServer.create();
 
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use("/api", router);
 server.post("/verify-tokopedia", (req, res) => {
