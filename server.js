9996fac017ef737ec36c3a56a529e5fdf861df55
azizrosyid <azizrosyid783@gmail.com>
2024-10-13 15:26:17 +0700

    Add endpoint for verifying Tokopedia invoice

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use("/api", router);
+server.post("/verify-tokopedia", (req, res) => {
+  const { invoiceId } = req.body;
+  if (!invoiceId) {
+    return res.status(400).json({ message: "Invoice ID is required" });
+  }
+
+  return res.json({ message: "Success", invoiceId });
+});
+
 server.listen(process.env.PORT || 8000, () => {
   console.log("JSON Server is running");
 });
